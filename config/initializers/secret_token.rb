ead1e9a43a1ee7ae8df6c5c95c48cc4a1e86bc4c
Dan Lichty <djlichty@gmail.com>
2011-01-04 17:48:44 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AmethystSprites::Application.config.secret_token = '87507633a1c5dd8e47d4c9bc5579bd6bc5c0f98451ffc9dedfc7df9928d014d38e6ebb62deb3ae73cfe84905057b26c9a52cd9f402721f07d0bde2cb31b67484'
