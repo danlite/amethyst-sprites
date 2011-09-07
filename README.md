c913a3876017ef34fe7b1040a2a07b02e44f9931
Dan Lichty <djlichty@gmail.com>
2011-09-07 12:30:07 -0400

    divify Pokemon gallery table

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 ## TODO
 
-- convert gallery from table into divs
 - add progress meter on gallery ("52% completed" or "380/730 completed")
 - user pages to show their contributions
 - user settings page to change email/password, set time zone
--- app/helpers/pokemon_helper.rb
+++ app/helpers/pokemon_helper.rb
@@ -1,24 +1,28 @@
 module PokemonHelper
   
   def html_for_series(series)
-    state_text = case series.state
-      when SERIES_RESERVED then "reserved"
-      when SERIES_WORKING then "being worked on"
-      when SERIES_AWAITING_EDIT then "awaiting edit"
-      when SERIES_EDITING then "being edited"
-      when SERIES_AWAITING_QC then "awaiting QC"
-      when SERIES_QC then "in QC"
-      when SERIES_AWAITING_APPROVAL then "awaiting approval"
-      when SERIES_DONE then "done"
-      when SERIES_ARCHIVED then "archived"
-      else "unknown state"
+    state_text = "available"
+    
+    if series
+      state_text = case series.state
+        when SERIES_RESERVED then "reserved"
+        when SERIES_WORKING then "being worked on"
+        when SERIES_AWAITING_EDIT then "awaiting edit"
+        when SERIES_EDITING then "being edited"
+        when SERIES_AWAITING_QC then "awaiting QC"
+        when SERIES_QC then "in QC"
+        when SERIES_AWAITING_APPROVAL then "awaiting approval"
+        when SERIES_DONE then "done"
+        when SERIES_ARCHIVED then "archived"
+        else "unknown state"
+      end
+    
+      state_text += " by #{series.reserver.name}" if series.owned?
+    
+      state_text += " (limbo)" if series.limbo?
     end
-    
-    state_text += " by #{series.reserver.name}" if series.owned?
-    
-    state_text += " (limbo)" if series.limbo?
       
-    state_text
+    content_tag(:p, state_text, :class => 'pokemon-state')
   end
   
   def html_for_available(pokemon, artist)
