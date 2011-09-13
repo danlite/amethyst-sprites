9d2dde85ebdf8fb8cdfe45abaeb792a7e0a67513
Dan Lichty <djlichty@gmail.com>
2011-09-13 19:41:07 -0400

    redesign gallery and activity feed: table layout and scrollable feed at top

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,15 @@
 module ApplicationHelper
   
+  GALLERY_COLUMNS = 5
+  
   def format_event_datetime(datetime)
     datetime.strftime("%b %e at %l:%M %p %Z")
   end
   
-  def series_button(series, text, sprite, small = false, style = '')
+  def series_button(series, text, sprite, small = false, options = {})
     dim = small ? 80 : 160
-    link_to(series_path(:id => series), :class => "button", :style => style) do
+    options[:class] = "button " + options[:class].to_s
+    link_to(series_path(:id => series), options) do
       things = []
       things << image_tag(sprite.image.url, :style => "height: #{dim}px; weight: #{dim}px; vertical-align: middle; padding: 4px; margin: 4px") if sprite
       things << tag(:br) if sprite and text
