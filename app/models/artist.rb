4b362f4235ad9353d5be7f8019c249a309c66324
Dan Lichty <djlichty@gmail.com>
2011-09-25 18:20:49 -0400

    assign new artists to contributors with the same name

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -6,6 +6,7 @@ class Artist < ActiveRecord::Base
   
   attr_accessor :password
   before_save :encrypt_password
+  after_create :assign_contributors
   
   has_many :reservations, :class_name => "SpriteSeries", :foreign_key => "reserver_id"
   has_many :sprites
@@ -22,13 +23,6 @@ class Artist < ActiveRecord::Base
       user
     else
       nil
-    end
-  end
-  
-  def encrypt_password
-    if password.present?
-      self.password_salt = BCrypt::Engine.generate_salt
-      self.password_hash = BCrypt::Engine.hash_secret(password, password_salt)
     end
   end
   
@@ -59,5 +53,18 @@ class Artist < ActiveRecord::Base
     end
     
     true    
+  end
+  
+  protected
+  
+  def assign_contributors
+    list = Contributor.where('artist_id IS NULL AND name = ?', self.name)
+  end
+  
+  def encrypt_password
+    if password.present?
+      self.password_salt = BCrypt::Engine.generate_salt
+      self.password_hash = BCrypt::Engine.hash_secret(password, password_salt)
+    end
   end
 end
