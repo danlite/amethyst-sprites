58e328cafa8b0e711a0e100f3a758d4b7648f5e4
Dan Lichty <djlichty@gmail.com>
2012-03-07 10:39:13 -0500

    update acted_on_at for Pokemon after changing their current series state

--- app/models/progress_activity.rb
+++ app/models/progress_activity.rb
@@ -2,4 +2,13 @@ class ProgressActivity < Activity
   validates :actor, :presence => true
   validates :series, :presence => true
   validates :subtype, :inclusion => { :in => SERIES_STATES }
+  
+  after_create :update_pokemon
+  
+  protected
+  
+  def update_pokemon
+    self.series.pokemon.update_attribute(:acted_on_at, Time.now) if self.subtype != SERIES_ARCHIVED
+  end
+
 end
